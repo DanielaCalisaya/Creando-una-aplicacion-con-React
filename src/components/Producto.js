2779be3147ddc66d125aef955b9f678aa915fa17
DanielaCalisaya <hdaniela127c@gmail.com>
2022-03-08 18:09:53 -0600

    TP with React-firts commit

---
+++ src/components/Producto.js
@@ -0,0 +1,30 @@
+/* import React from 'react'
+
+const Producto = () => {
+  return (
+    <main class="productos"> 
+    <h2 id="productos">Nuestros productos</h2>
+    <article>
+       <img src="img/reloj1.jpg" alt="Reloj">
+       <h2>Lorem, ipsum.</h2>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni mollitia blanditiis natus dicta error, cum alias ipsam libero odio impedit, fuga at dolore voluptatem illum pariatur cupiditate fugit, ullam amet?</p>
+        <h2>Precio: $450</h2>
+        <button>Ver más</button>
+    </article>
+    <article>
+        <img src="img/reloj2.jpg" alt="Reloj">
+       <h2>Lorem, ipsum.</h2>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni mollitia blanditiis natus dicta error, cum alias ipsam libero odio impedit, fuga at dolore voluptatem illum pariatur cupiditate fugit, ullam amet?</p>
+        <h2>Precio: $650</h2>
+        <button>Ver más</button>
+    </article>
+voluptatem illum pariatur cupiditate fugit, ullam amet?</p>
+        <h2>Precio: $999</h2>
+        <button>Ver más</button>
+    </article>
+</main>
+
+  )
+}
+
+export default Producto */
---
+++ src/components/Web.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Encabezado } from './Encabezado';
+function Web(){
+  return (
+    <div className="container">
+      <Encabezado/>
+      <Banner/>
+      <Producto/>
+    </div>
+  )
+
+}
+export default Web;
